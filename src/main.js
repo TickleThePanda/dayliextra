cd47e9147b0b723688f6002269b008b8837e87fc
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2023-05-22 18:05:56 +0100

    Fix spaces

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { PassThrough } from "stream";
 const file = process.argv[2];
 
 if (file === undefined) {
-    throw new Error("You must specify a file as the first argument.");
+  throw new Error("You must specify a file as the first argument.");
 }
 
 console.log(file);
@@ -14,22 +14,21 @@ const backup = await extractBackup(file);
 
 console.log(Object.keys(backup));
 
-
 async function extractBackup(fileName) {
-    const stream = fs.createReadStream(fileName)
-        .pipe(unzipper.ParseOne(/backup.daylio/))
-        .pipe(new PassThrough());
+  const stream = fs
+    .createReadStream(fileName)
+    .pipe(unzipper.ParseOne(/backup.daylio/))
+    .pipe(new PassThrough());
 
-    const chunks = [];
+  const chunks = [];
 
-    for await (const chunk of stream) {
-        chunks.push(Buffer.from(chunk));
-    }
+  for await (const chunk of stream) {
+    chunks.push(Buffer.from(chunk));
+  }
 
-    const base64 = Buffer.concat(chunks).toString("utf-8");
+  const base64 = Buffer.concat(chunks).toString("utf-8");
 
-    const text = Buffer.from(base64, 'base64');
+  const text = Buffer.from(base64, "base64");
 
-    return JSON.parse(text);
-
+  return JSON.parse(text);
 }
