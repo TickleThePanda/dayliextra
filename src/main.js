11d49bc35797de382e9672b67ef4ea6962421261
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2023-05-22 18:02:20 +0100

    Add data extractor

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import unzipper from "unzipper";
+import fs from "fs";
+import { PassThrough } from "stream";
+
+const file = process.argv[2];
+
+if (file === undefined) {
+    throw new Error("You must specify a file as the first argument.");
+}
+
+console.log(file);
+
+const backup = await extractBackup(file);
+
+console.log(Object.keys(backup));
+
+
+async function extractBackup(fileName) {
+    const stream = fs.createReadStream(fileName)
+        .pipe(unzipper.ParseOne(/backup.daylio/))
+        .pipe(new PassThrough());
+
+    const chunks = [];
+
+    for await (const chunk of stream) {
+        chunks.push(Buffer.from(chunk));
+    }
+
+    const base64 = Buffer.concat(chunks).toString("utf-8");
+
+    const text = Buffer.from(base64, 'base64');
+
+    return JSON.parse(text);
+
+}
